89b548c2892cefb6ed4e1fe94dcbe09f45c84407
pine <916931772@qq.com>
2017-06-12 00:34:37 +0800

    commons

---
+++ jsunday-mybatis3/mybatis3-mysql/src/test/java/com/zingson/mybatis/MysqlTest.java
@@ -0,0 +1,43 @@
+package com.zingson.mybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Mybatis mysql test
+ */
+public class MysqlTest {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    SqlSessionFactory sqlSessionFactory;
+
+    @Before
+    public void createSqlSessionFactory(){
+        try {
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void openSession(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        log.info("SqlSession={}",sqlSession);
+    }
+
+
+
+}
